efa9d109a36e63c47697f41d31eaf41f3f137dff
Alex Dima <alexdima@microsoft.com>
2016-05-17 10:44:11 +0200

    initial editor.d.ts

---
+++ monaco.editor.contribution.ts
@@ -0,0 +1,16 @@
+
+(function() {
+
+this.MonacoPlugins = this.MonacoPlugins || [];
+this.MonacoPlugins.push({
+	language: {
+		id: 'typescript',
+		extensions: ['.ts'],
+		aliases: ['TypeScript', 'ts', 'typescript'],
+		mimetypes: ['text/typescript']
+	}
+});
+
+
+	
+})();

807f3f1f805525ab6e1c5f5ca8572115ed9ddee7
Johannes Rieken <johannes.rieken@gmail.com>
2016-05-26 13:14:24 +0200

    one round of feedback

--- monaco-editor.d.ts
+++ monaco-editor.d.ts
@@ -13,9 +13,11 @@ declare module monaco.worker {
 
 declare module monaco.languages {
 
-
+    //@alex - I'd call this setLanguageConfiguration cos there is only 1 per language id
     export function registerLanguageConfiguration(languageId: string, configuration: IRichEditConfiguration): IDisposable;
 
+    //@alex - unsure but I think it's also only 1 and the setTokensProvider
+    //@alex - unsure if TokenProvider is a good name, at least alig function name and 'support' name
     export function registerTokensProvider(languageId: string, support: ITokenizationSupport2): IDisposable;
 
     export function registerReferenceProvider(languageId: string, support: ReferenceProvider): IDisposable;
@@ -48,6 +50,7 @@ declare module monaco.languages {
 
     export function register(language: ILanguageExtensionPoint): void;
 
+    // @alex should this be eventish? use Event<T>, call it onWillLoadLanguage?
     export function onLanguage(languageId: string, callback: () => void): IDisposable;
 
     export interface CommentRule {
@@ -103,11 +106,13 @@ declare module monaco.languages {
         getId(): string;
     }
 
+    // @alex IXYZ2? is the counter still needed?
     export interface IToken2 {
         startIndex: number;
         scopes: string | string[];
     }
 
+    // @alex same
     export interface ILineTokens2 {
         tokens: IToken2[];
         endState: IState2;
@@ -119,11 +124,13 @@ declare module monaco.languages {
         equals(other: IState2): boolean;
     }
 
+    // @alex same
     export interface ITokenizationSupport2 {
         getInitialState(): IState2;
         tokenize(line: string, state: IState2): ILineTokens2;
     }
 
+    // @alex same
     /**
      * A hover represents additional information for a symbol or word. Hovers are
      * rendered in a tooltip-like widget.
@@ -353,7 +360,8 @@ declare module monaco.languages {
 
 declare module monaco.editor {
 
-
+    // @alex We could think about exposing the service collection here (https://github.com/Microsoft/vscode/blob/master/src/vs/platform/instantiation/common/serviceCollection.ts#L13)
+    // with that we have service identifier end to end and no more service names.
     export function setupServices(services: IEditorOverrideServices): IEditorOverrideServices;
 
     export function create(domElement: HTMLElement, options: IEditorConstructionOptions, services: IEditorOverrideServices): ICodeEditor;
@@ -404,9 +412,11 @@ declare module monaco.editor {
         mode?: string;
     }
 
+    // @alex empty interfaces?!
     export interface IDiffEditorConstructionOptions extends IDiffEditorOptions {
     }
 
+    // @alex empty interfaces?!
     export interface IEditorOverrideServices {
     }
 
@@ -479,7 +489,7 @@ declare module monaco {
         (progress: any): any;
     }
 
-
+    // @alex unsure about the name - it can be confused with the native promise.
     export class Promise<V> {
 
         constructor(init: (complete: TValueCallback<V>, error: (err: any) => void, progress: ProgressCallback) => void, oncancel?: any);
@@ -2341,6 +2351,7 @@ declare module monaco.editor {
         onDidChangeOptions(listener: (e: IModelOptionsChangedEvent) => void): IDisposable;
         onDidChangeMode(listener: (e: IModelModeChangedEvent) => void): IDisposable;
         onWillDispose(listener: () => void): IDisposable;
+        // @alex should we expose that?
         addBulkListener(listener: BulkListenerCallback): IDisposable;
         /**
          * A unique identifier associated with this model.
@@ -2408,7 +2419,8 @@ declare module monaco.editor {
          */
         newMode: languages.IMode;
     }
-
+    // @alex can we only expose this event to describe
+    // model modifications?
     /**
      * An event describing a change in the text of a model.
      */

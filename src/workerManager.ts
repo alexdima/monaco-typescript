7138e19291ac658e0d3607a64d21e84c5e494649
Alex Dima <alexdima@microsoft.com>
2016-06-09 12:00:39 +0200

    Stop web worker when being IDLE for 2 min

--- src/workerManager.ts
+++ src/workerManager.ts
@@ -11,75 +11,98 @@ import Promise = monaco.Promise;
 import IDisposable = monaco.IDisposable;
 import Uri = monaco.Uri;
 
+const STOP_WHEN_IDLE_FOR = 2 * 60 * 1000; // 2min
+
 export class WorkerManager {
 
 	private _defaults: LanguageServiceDefaults;
+	private _idleCheckInterval: number;
+	private _lastUsedTime: number;
+	private _configChangeListener: IDisposable;
+
 	private _worker: monaco.editor.MonacoWebWorker<TypeScriptWorker>;
 	private _client: Promise<TypeScriptWorker>;
 
 	constructor(defaults: LanguageServiceDefaults) {
 		this._defaults = defaults;
 		this._worker = null;
+		this._idleCheckInterval = setInterval(() => this._checkIfIdle(), 30 * 1000);
+		this._lastUsedTime = 0;
+		this._configChangeListener = this._defaults.onDidChange(() => this._stopWorker());
 	}
 
-	private _createClient(): Promise<TypeScriptWorker> {
-		// TODO: stop when idle
-		this._worker = monaco.editor.createWebWorker<TypeScriptWorker>({
-			moduleId: 'vs/language/typescript/src/worker',
-		});
-
-		let configChangeListener: IDisposable = null;
-
-		const stopWorker = () => {
-			configChangeListener.dispose();
+	private _stopWorker(): void {
+		if (this._worker) {
 			this._worker.dispose();
 			this._worker = null;
-			this._client = null;
-		};
-
-		configChangeListener = this._defaults.onDidChange(stopWorker);
-
-		let _client:TypeScriptWorker = null;
-		return this._worker.getProxy().then((client) => {
-			_client = client;
-		}).then(_ => {
-			const {compilerOptions, extraLibs} = this._defaults;
-			return _client.acceptDefaults(compilerOptions, extraLibs);
-		}).then(_ => _client);
-
-		// // stop worker after being idle
-		// const handle = setInterval(() => {
-		// 	if (Date.now() - client.getLastRequestTimestamp() > 1000 * 60) {
-		// 		stopWorker();
-		// 	}
-		// }, 1000 * 60);
-		// this._clientDispose.push({ dispose() { clearInterval(handle); } });
-
-		// // stop worker when defaults change
-		// this._clientDispose.push(this._defaults.onDidChange(() => stopWorker()));
-
-		// // send default to worker right away
-		// const worker = client.get();
-		// const {compilerOptions, extraLibs} = this._defaults;
-		// return worker.acceptDefaults(compilerOptions, extraLibs).then(() => ({ worker, manager }));
+		}
+		this._client = null;
 	}
 
 	dispose(): void {
-		console.log('I SHOULD DISPOSE??!?!!?');
-	// 	this._clientDispose = dispose(this._clientDispose);
-	// 	this._client = null;
+		clearInterval(this._idleCheckInterval);
+		this._configChangeListener.dispose();
+		this._stopWorker();
+	}
+
+	private _checkIfIdle(): void {
+		if (!this._worker) {
+			return;
+		}
+		let timePassedSinceLastUsed = Date.now() - this._lastUsedTime;
+		if (timePassedSinceLastUsed > STOP_WHEN_IDLE_FOR) {
+			this._stopWorker();
+		}
+	}
+
+	private _getClient(): Promise<TypeScriptWorker> {
+		this._lastUsedTime = Date.now();
+
+		if (!this._client) {
+			this._worker = monaco.editor.createWebWorker<TypeScriptWorker>({
+				moduleId: 'vs/language/typescript/src/worker',
+			});
+
+			let _client:TypeScriptWorker = null;
+
+			// avoid cancellation
+			this._client = toShallowCancelPromise(
+				this._worker.getProxy().then((client) => {
+					_client = client;
+				}).then(_ => {
+					const {compilerOptions, extraLibs} = this._defaults;
+					return _client.acceptDefaults(compilerOptions, extraLibs);
+				}).then(_ => _client)
+			);
+		}
+
+		return this._client;
 	}
 
 	getLanguageServiceWorker(...resources: Uri[]): Promise<TypeScriptWorker> {
-		if (!this._client) {
-			this._client = this._createClient();
-		}
-
-		// create a new promise to avoid cancellation
-		return new monaco.Promise((c, e) => {
-			this._client.then((data) => {
-			return this._worker.withSyncedResources(resources).then(_ => data)
-			}).then(c, e);
-		}, () => {});
+		let _client:TypeScriptWorker;
+		return this._getClient().then((client) => {
+			_client = client
+		}).then(_ => {
+			return this._worker.withSyncedResources(resources)
+		}).then(_ => _client);
 	}
+}
+
+function toShallowCancelPromise<T>(p:Promise<T>): Promise<T> {
+	let completeCallback: (value:T)=>void;
+	let errorCallback: (err:any)=>void;
+
+	let r = new Promise<T>((c, e) => {
+		completeCallback = c;
+		errorCallback = e;
+	}, () => {
+		// cancel this promise but not the
+		// outer promise
+		errorCallback(new Error('Canceled'));
+	});
+
+	p.then(completeCallback, errorCallback);
+
+	return r;
 }
